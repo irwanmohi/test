27e21746800a64c853dcee472214a7f13fdb996a
mohd irwan bin mohi <48343630+irwanmohi@users.noreply.github.com>
2021-08-14 01:33:25 +0800

    Update ocsfree.sh

--- ocsfree.sh
+++ ocsfree.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Script by : _Dreyannz_
-apt-get update
-apt-get install apache2 -y
-apt-get install libssh2-php -y
+
+# install webserver extensions
+apt-get -y install nginx
+apt-get -y install php7.0-fpm php7.0-cli libssh2-1 php-ssh2 php7.0 apache2
 service apache2 restart
-apt-get install nginx -y
-apt-get install php-fpm -y
-apt-get install php-cli -y
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Dreyannz/AutoScriptVPS/master/Files/Nginx/nginx.conf"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Dreyannz/AutoScriptVPS/master/Files/Nginx/vps.conf"
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-service php5-fpm restart
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/lanundarat87/xxx/main/Res/Other/nginx.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/lanundarat87/xxx/main/Res/Other/vps.conf"
+wget -O /etc/nginx/conf.d/monitoring.conf "https://raw.githubusercontent.com/lanundarat87/xxx/main/Res/Other/monitoring.conf"
+sed -i 's/listen = \/run\/php\/php7.0-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.0/fpm/pool.d/www.conf
+sed -i $MYIP2 /home/vps/public_html/index.php;
+service php7.0-fpm restart
 service nginx restart
 cd /home/vps/public_html
-wget https://raw.githubusercontent.com/Dreyannz/VPS_Site/master/Version%201.0/index.php
-wget --quiet https://raw.githubusercontent.com/Dreyannz/VPS_Site/master/Version%201.0/v1_server_details.php
+wget https://raw.githubusercontent.com/Dreyannz/VPS_Site/master/Version%203.0/index.php
+wget --quiet https://raw.githubusercontent.com/Dreyannz/VPS_Site/master/Version%203.0/server.php
