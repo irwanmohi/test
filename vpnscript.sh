b16accab58d37dd3b778736a5b2289c4651734b7
mohd irwan bin mohi <48343630+irwanmohi@users.noreply.github.com>
2021-08-18 04:08:59 +0800

    Update vpnscript.sh

--- vpnscript.sh
+++ vpnscript.sh
@@ -296,15 +296,15 @@ cd
 chown -R mysql:mysql /var/lib/mysql/
 chmod -R 755 /var/lib/mysql/
 
-apt-get -y install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
+apt-get -y install nginx php php-fpm php-cli php-mysql php-mcrypt
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup 
 mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup 
 wget -O /etc/nginx/nginx.conf "http://script.hostingtermurah.net/repo/blog/ocspanel-debian7/nginx.conf" 
 wget -O /etc/nginx/conf.d/vps.conf "http://script.hostingtermurah.net/repo/blog/ocspanel-debian7/vps.conf" 
-sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini 
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/fpm/php.ini 
+sed -i 's/listen = \/var\/run\/php7.3-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 useradd -m vps
 mkdir -p /home/vps/public_html
@@ -312,8 +312,8 @@ rm /home/vps/public_html/index.html
 rm /home/vps/public_html/index.php
 echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
-chmod -R g+rw /home/vps/public_html service php5-fpm restart
-service php5-fpm restart
+chmod -R g+rw /home/vps/public_html service php7.3-fpm restart
+service php7.3-fpm restart
 service nginx restart
 
 apt-get -y install zip unzip
